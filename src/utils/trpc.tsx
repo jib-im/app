d20a8b121f7a57e175cbb2c0fb9ef2f1ce213f20
Brice Suazo <bricesuazo@gmail.com>
2022-12-27 14:12:40 +0800

    Comment unused createTRPCNext

--- src/utils/trpc.tsx
+++ src/utils/trpc.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { httpBatchLink, loggerLink } from "@trpc/client";
-import { createTRPCNext } from "@trpc/next";
+// import { createTRPCNext } from "@trpc/next";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import superjson from "superjson";
 
