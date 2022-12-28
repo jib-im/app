b26bfc19b433109505e9cecdf72fa177d4e21b2f
Brice Suazo <bricesuazo@gmail.com>
2022-12-29 00:37:04 +0800

    Fix mobile responsiveness in Link component.

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -14,16 +14,18 @@ import Link from "next/link";
 
 const Links = () => {
   return (
-    <div className="flex items-center justify-between rounded-lg bg-gray-800 px-4 py-6 transition-all hover:bg-gray-700/50">
+    <div className="flex items-center justify-between gap-x-2 rounded-lg bg-gray-800 px-4 py-6 transition-all hover:bg-gray-700/50">
       <div className="">
         <div className="flex items-center gap-x-2 text-gray-300">
           <Link href="https://slash.ly/github">
-            <h4 className="text-blue-500">slash.ly/github</h4>
+            <h4 className="text-sm text-blue-500 sm:text-base">
+              slash.ly/github
+            </h4>
           </Link>
-          <div className="cursor-pointer rounded-full bg-gray-700 p-2 transition-all hover:scale-110 hover:bg-gray-600 hover:text-blue-200">
-            <FaRegCopy className="h-3 w-3" />
+          <div className="hidden cursor-pointer rounded-full bg-gray-700 p-2 transition-all hover:scale-110 hover:bg-gray-600 hover:text-blue-200 sm:block">
+            <FaRegCopy className="h-3 w-3 " />
           </div>
-          <div className="cursor-pointer rounded-full bg-gray-700 p-2 transition-all hover:scale-110 hover:bg-gray-600 hover:text-blue-200">
+          <div className="hidden cursor-pointer rounded-full bg-gray-700 p-2 transition-all hover:scale-110 hover:bg-gray-600 hover:text-blue-200 sm:block">
             <FaQrcode className="h-3 w-3" />
           </div>
           <Link
@@ -31,13 +33,17 @@ const Links = () => {
             className="flex items-center gap-x-2 rounded-md bg-gray-700 px-2 py-1 transition-all hover:scale-105 hover:bg-gray-600 hover:text-blue-200"
           >
             <FaRegChartBar className="h-3 w-3" />
-            <p className="text-xs">2 clicks</p>
+            <p className="truncate text-xs">2 clicks</p>
           </Link>
         </div>
-        <p className="text-sm text-gray-300">https://github.com/slash-ly</p>
+        <p className="w-52 truncate text-xs text-gray-300 sm:w-96 sm:text-sm md:w-[28rem] lg:w-[32rem]">
+          https://github.com/slash-ly
+        </p>
       </div>
       <div className="relative flex items-center gap-x-2 text-sm text-gray-400">
-        <p>5 hrs ago</p>
+        <p className="hidden truncate text-sm leading-none sm:block">
+          5 hrs ago
+        </p>
         <Menu>
           <Menu.Button className="rounded-md px-1.5 py-2 transition-colors hover:bg-gray-500">
             <FaEllipsisV />
