7e82adc0d2204b56daff2f470f1d6e2eb625cb2a
Brice Suazo <bricesuazo@gmail.com>
2022-12-29 17:40:30 +0800

    Fix hydration fail in footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { FaGithub, FaTwitter } from "react-icons/fa";
+import { useHydrationFailedHack } from "../hooks/useHydrationFailedHack";
 
 const Footer = () => {
+  const { mounted } = useHydrationFailedHack();
+  if (!mounted) return null;
+
   return (
     <footer className="border-t border-gray-800">
       <section className="mx-auto flex max-w-screen-lg items-center justify-center gap-x-8 p-4 text-gray-300">
@@ -14,9 +19,14 @@ const Footer = () => {
         </Link>
         <Link
           href="https://slash.ly"
-          className="relative h-8 w-8 overflow-hidden rounded-full border border-gray-700/50 transition-opacity hover:opacity-75"
+          className="relative overflow-hidden rounded-full border border-gray-700/50 transition-opacity hover:opacity-75"
         >
-          <Image src="/images/slash-logo.png" alt="Slash.ly Logo" fill />
+          <Image
+            src="/images/slash-logo.png"
+            alt="Slash.ly Logo"
+            width={32}
+            height={32}
+          />
         </Link>
         <Link
           href="https://slash.ly/github"
