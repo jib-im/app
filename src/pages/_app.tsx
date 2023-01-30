2f26f808474f49777b93434c5cd650c3ffc0eb94
Brice Suazo <bricesuazo@gmail.com>
2023-01-30 19:07:39 +0800

    Remigrate to T3.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,20 @@
-import "../styles/globals.css";
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { Analytics } from "@vercel/analytics/react";
-import { Poppins } from "@next/font/google";
-import { trpc } from "../utils/trpc";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import { useRouter } from "next/router";
 
-const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-poppins",
-});
+import { api } from "../utils/api";
+
+import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-  const { pathname } = useRouter();
-
   return (
     <SessionProvider session={session}>
-      <main className={`${poppins.variable} font-sans`}>
-        {pathname !== "/login" && <Header session={session} />}
-        <Component {...pageProps} />
-        <Analytics />
-        {pathname !== "/login" && <Footer />}
-      </main>
+      <Component {...pageProps} />
     </SessionProvider>
   );
 };
 
-export default trpc.withTRPC(MyApp);
+export default api.withTRPC(MyApp);
--- src/pages/welcome.tsx
+++
@@ -1,7 +0,0 @@
-const Welcome = () => {
-  return <div>Welcome new user!</div>;
-};
-
-export default Welcome;
-
-export { default as getServerSideProps } from "../lib/serverProps";
